d457603bde6e7e6d47be5f44865cc31aee1e8a8a
Omar Alshaali <omar.alshaali@gmail.com>
2020-02-26 16:43:06 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# This is your main function
+# This is where you declare your variables and use your functions
+def main():
+	print("Hello")
+
+# Call your main function    
+if __name__ == "__main__":
+    main()
